e62eb5ee4f7fff2c5967182f60f895dcf78bdd39
unknown <dpfflsk45@gmail.com>
2022-05-23 10:44:45 +0900

    chore: 초기 셋팅 완료

---
+++ src/assets/index.js
@@ -0,0 +1,9 @@
+export { ReactComponent as AlarmIcon } from './alarm.svg';
+export { ReactComponent as DashBoardIcon } from './dashboard.svg';
+export { ReactComponent as Logo } from './logo.svg';
+export { ReactComponent as GraphIcon } from './graph.svg';
+export { ReactComponent as ProfileIcon } from './profile.svg';
+export { ReactComponent as SettingIcon } from './setting.svg';
+export { ReactComponent as GuidepIcon } from './guide.svg';
+export { ReactComponent as ArrowDownIcon } from './arrowDown.svg';
+export { ReactComponent as ArrowUpIcon } from './svg/arrowUp.svg';
