c7e98aa24f6452e887bcc3ee070d93cb0d220442
유재민 <yoojamer@gmail.com>
2022-05-24 14:24:38 +0900

    매체 현황 구현 중

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,13 @@
+import MediaStatus from './MediaStatus';
+
 import styles from './Routes.module.scss';
 
 const App = () => {
-  return <div className={styles.app} />;
+  return (
+    <div className={styles.app}>
+      <MediaStatus />
+    </div>
+  );
 };
 
 export default App;
