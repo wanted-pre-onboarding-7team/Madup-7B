a8f9f88060bfe264862476d142928d2c77a63156
unknown <dpfflsk45@gmail.com>
2022-05-24 14:40:54 +0900

    feat: victory.js 그래프 구상중

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
 
 import './styles/index.scss';
 
@@ -8,9 +9,9 @@ import Routes from './routes';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <Routes />
-  </React.StrictMode>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,13 @@
 import styles from './Routes.module.scss';
+import { Routes, Route } from 'react-router-dom';
+import DashBoardChart from '../components/DashBoardChart/index';
 
 const App = () => {
-  return <div className={styles.app} />;
+  return (
+    <div className={styles.app}>
+      <DashBoardChart />
+    </div>
+  );
 };
 
 export default App;
