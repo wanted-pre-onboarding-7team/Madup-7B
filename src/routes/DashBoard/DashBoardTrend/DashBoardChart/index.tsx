7ac9fb142bb2ad73d88d10ac2f1b357e03a69e83
unknown <starberry970310@gmail.com>
2022-05-26 04:38:23 +0900

    fix: 드롭다운 상태 저장

--- src/routes/DashBoard/DashBoardTrend/DashBoardChart/index.tsx
+++ src/routes/DashBoard/DashBoardTrend/DashBoardChart/index.tsx
@@ -6,7 +6,6 @@ import {
   VictoryGroup,
   VictoryTooltip,
   VictoryLabel,
-  VictoryCursorContainer,
 } from 'victory';
 import dayjs from 'dayjs';
 
@@ -79,19 +78,14 @@ const DashBoardChart = () => {
               }}
               style={{ data: { fill: 'transparent' } }}
               size={5}
-              containerComponent={<VictoryCursorContainer cursorDimension='x' />}
               labels={({ datum }) => `${datum.y}`}
               labelComponent={
                 <VictoryTooltip
-                  style={{ fill: 'white', fontSize: 20, textAnchor: 'middle' }}
-                  flyoutStyle={{
-                    stroke: '#3a474e',
-                    fill: '#3a474e',
-                    margin: 10,
-                  }}
+                  style={{ fill: '#ffffff', stroke: 'none', fontSize: 15, textAnchor: 'middle' }}
+                  flyoutStyle={{ stroke: '#ffffff', fill: '#3a474e', strokeWidth: 0, margin: 10 }}
                   flyoutWidth={100}
-                  dx={60}
-                  dy={60}
+                  dx={50}
+                  dy={50}
                 />
               }
               events={[
