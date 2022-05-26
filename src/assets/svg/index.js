330adffb5fc08040c4f701285717f30e491c78a2
unknown <starberry970310@gmail.com>
2022-05-26 10:34:14 +0900

    chore: delete ArrowUpIcon

--- src/assets/svg/index.js
+++ src/assets/svg/index.js
@@ -6,7 +6,6 @@ export { ReactComponent as ProfileIcon } from './profile.svg';
 export { ReactComponent as SettingIcon } from './setting.svg';
 export { ReactComponent as GuidepIcon } from './guide.svg';
 export { ReactComponent as ArrowDownIcon } from './arrowDown.svg';
-export { ReactComponent as ArrowUpIcon } from './arrowUp.svg';
 export { ReactComponent as PlusIcon } from './plus.svg';
 export { ReactComponent as LoadingInner } from './loadingInner.svg';
 export { ReactComponent as LoadingOuter } from './loadingOuter.svg';
